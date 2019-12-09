18deec679dca8243206c3af58bbaaa51b4b0d846
taslimy <taslimer@gmail.com>
2019-12-09 16:22:10 -0500

    added a seperate toggler component to show / reveal answer to questions

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,23 +2,19 @@ import React from "react";
 import "./card.styles.css";
 
 import QUESTIONS_DATA from "../../assets/data/questions.data";
+import ToggleQuestion from "../toggle-questions/togglequestion.component";
 
 class Card extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      showAnswer: false
-    };
-  }
-
   render() {
     return (
-      <div className='card-container'>
+      <div className="card-container">
         <h2>Questions</h2>
         {QUESTIONS_DATA.frontend.questions.map(frontend => (
           <div className="card" key={frontend.id}>
-            <h2>{frontend.question}</h2>
-            <p>{frontend.answer}</p>
+            <ToggleQuestion
+              question={frontend.question}
+              answer={frontend.answer}
+            />
           </div>
         ))}
       </div>
---
+++ src/components/toggle-questions/togglequestion.component.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class ToggleQuestion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isHidden: true };
+  }
+
+  toggleHidden = () => {
+    this.setState({ isHidden: !this.state.isHidden });
+  };
+
+  render() {
+    const { question, answer } = this.props;
+    const { isHidden } = this.state;
+    return (
+      <div>
+        <h2>{question}</h2>
+        {!isHidden && <p>{answer}</p>}
+        <button onClick={this.toggleHidden}>Reveal Answer</button>
+      </div>
+    );
+  }
+}
+export default ToggleQuestion;
